ab150cdd434034b4243c8edfc79113faa041d6cb
Topson <swq201165@gmail.com>
2018-11-11 21:01:54 +0800

    MultiAuth with laravel 5.7

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'topson',
+            'email' => 'swq201165@gmail.com',
+            'password'=> bcrypt('123456'),
+        ]);
+    }
+} 
